0fc9cbedd80c9174345d15975a1aa7f3e325fd9d
tester22 <anton@trugen.net>
2017-01-22 13:44:04 +0100

    Added support for Mute

--- Sonos/plugin.py
+++ Sonos/plugin.py
@@ -30,6 +30,7 @@ import xml.etree.ElementTree as ET
 playerState = 0
 mediaLevel = 0
 mediaDescription = ""
+muted = 2
 
 # Domoticz call back functions
 def onStart():
@@ -44,6 +45,9 @@ def onStart():
     elif (Parameters["Mode2"] == "Volume" and 2 not in Devices):
         Domoticz.Device(Name="Volume",  Unit=2, Type=244, Subtype=73, Switchtype=7,  Image=8).Create()
         Domoticz.Log("Volume device created.")
+    elif (Parameters["Mode2"] != "Volume" and 2 in Devices):
+        Devices[2].Delete()
+        Domoticz.Log("Volume device deleted.")
     else:
         if (1 in Devices): playerState = Devices[1].nValue
         if (2 in Devices): mediaLevel = Devices[2].nValue
@@ -63,12 +67,13 @@ def onStart():
 def onConnect(Status, Description):
     return True
 
-def onMessage(Data):
-    global playerState, mediaDescription, mediaLevel
+def onMessage(Data, Status, Extra):
+    global playerState, mediaDescription, mediaLevel, muted
     xmltree = ET.fromstring(Data)
     sonosPlayRespone = xmltree.find('.//{urn:schemas-upnp-org:service:AVTransport:1}PlayResponse')
     sonosPauseRespone = xmltree.find('.//{urn:schemas-upnp-org:service:AVTransport:1}PauseResponse')
     sonosVolumeResponse = xmltree.findtext('.//{urn:schemas-upnp-org:service:RenderingControl:1}GetVolumeResponse/CurrentVolume')
+    sonosMuteResponse = xmltree.findtext('.//{urn:schemas-upnp-org:service:RenderingControl:1}GetMuteResponse/CurrentMute')
     sonosSetVolumeResponse = xmltree.findtext('.//{urn:schemas-upnp-org:service:RenderingControl:1}SetVolumeResponse')
     sonosState = xmltree.findtext('.//CurrentTransportState')
     metaData = xmltree.findtext('.//TrackMetaData')
@@ -77,15 +82,23 @@ def onMessage(Data):
         UpdateDevice(1, playerState, mediaDescription)
     elif sonosPauseRespone != None:
         playerState = 0
+        mediaDescription = ''
         UpdateDevice(1, playerState, mediaDescription)
     elif sonosVolumeResponse != None:
         mediaLevel = sonosVolumeResponse
-        UpdateDevice(2, 2, mediaLevel)
+        UpdateDevice(2, muted, mediaLevel)
+    elif sonosMuteResponse != None:
+        if sonosMuteResponse == "1":
+            muted = 0
+        else:
+            muted = 2
+        UpdateDevice(2, muted, mediaLevel)
     elif sonosState != None:
         if sonosState == 'PLAYING':
             playerState = 1
         if sonosState == 'PAUSED_PLAYBACK' or sonosState == 'STOPPED':
             playerState = 0
+            mediaDescription = ''
         UpdateDevice(1, playerState, mediaDescription)
     elif metaData == "NOT_IMPLEMENTED":
         mediaDescription = "Grouped"
@@ -110,22 +123,31 @@ def onCommand(Unit, Command, Level, Hue):
     action, sep, params = Command.partition(' ')
     action = action.capitalize()
 
-    if (action == 'On') or (action == 'Play'):
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Play', "/MediaRenderer/AVTransport/Control")
-    elif (action == 'Pause') or (action == 'Off'):
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Pause xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Pause></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Pause', "/MediaRenderer/AVTransport/Control")
-    elif (action == 'Stop'):
-        sendMessage('urn:schemas-upnp-org:service:AVTransport:1#Stop', '<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Stop xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Stop></s:Body></s:Envelope>', "/MediaRenderer/AVTransport/Control")
-    elif (action == 'Next'):
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Next xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Next></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Next', "/MediaRenderer/AVTransport/Control")
-    elif (action == 'Previous'):
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Previous xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Previous></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Previous', "/MediaRenderer/AVTransport/Control")
-    elif (action == 'Set') and ((params.capitalize() == 'Level') or (Command.lower() == 'Volume')):
-        mediaLevel = Level
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:SetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>' + str(mediaLevel) +  '</DesiredVolume></u:SetVolume></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#SetVolume', "/MediaRenderer/RenderingControl/Control")
-        # Send an update request to get updated data from the player
-        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetVolume></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#GetVolume', "/MediaRenderer/RenderingControl/Control")
 
+    if (Unit == 1):  # Playback control
+        if (action == 'On') or (action == 'Play'):
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Play', "/MediaRenderer/AVTransport/Control")
+        elif (action == 'Pause') or (action == 'Off'):
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Pause xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Pause></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Pause', "/MediaRenderer/AVTransport/Control")
+        elif (action == 'Stop'):
+            sendMessage('urn:schemas-upnp-org:service:AVTransport:1#Stop', '<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Stop xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Stop></s:Body></s:Envelope>', "/MediaRenderer/AVTransport/Control")
+        elif (action == 'Next'):
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Next xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Next></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Next', "/MediaRenderer/AVTransport/Control")
+        elif (action == 'Previous'):
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Previous xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Previous></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#Previous', "/MediaRenderer/AVTransport/Control")
+    if (Unit == 2):  # Volume control
+        if (action == 'Set') and ((params.capitalize() == 'Level') or (Command.lower() == 'Volume')):
+            mediaLevel = Level
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:SetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>' + str(mediaLevel) +  '</DesiredVolume></u:SetVolume></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#SetVolume', "/MediaRenderer/RenderingControl/Control")
+            # Send an update request to get updated data from the player
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetVolume></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#GetVolume', "/MediaRenderer/RenderingControl/Control")
+        elif (action == 'On') or (action == 'Off'):
+            if action == 'On':
+                DesiredMute = "0"
+            else:
+                DesiredMute = "1"
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:SetMute xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel><DesiredMute>' + DesiredMute + '</DesiredMute></u:SetMute></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#SetMute', "/MediaRenderer/RenderingControl/Control")
+            sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetMute xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetMute></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#GetMute', "/MediaRenderer/RenderingControl/Control")
     return True
 
 def onNotification(Data):
@@ -139,6 +161,7 @@ def onHeartbeat():
         sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetPositionInfo xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetPositionInfo></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:AVTransport:1#GetPositionInfo', "/MediaRenderer/AVTransport/Control")
     if Parameters["Mode2"] == "Volume":
         sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetVolume xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetVolume></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#GetVolume', "/MediaRenderer/RenderingControl/Control")
+        sendMessage('<?xml version="1.0"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetMute xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"><InstanceID>0</InstanceID><Channel>Master</Channel></u:GetMute></s:Body></s:Envelope>', 'urn:schemas-upnp-org:service:RenderingControl:1#GetMute', "/MediaRenderer/RenderingControl/Control")
     return True
 
 def onDisconnect():
@@ -161,7 +184,7 @@ def sendMessage(data, method, url):
     
     if response.status == 200:
         data = response.read().decode("utf-8")
-        onMessage(data)
+        onMessage(data, "", "")
     return
 
 # Device specific functions
